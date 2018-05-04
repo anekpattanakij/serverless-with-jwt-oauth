b2aaf786ac8b42691ae50c75fc66068fabf923cd
Unknown <a.anekpattanakij@gmail.com>
2018-05-04 21:38:48 +0700

    Add error util for checking network error

--- src/common/error.ts
+++ src/common/error.ts
@@ -1,22 +1,36 @@
 import { BaseCustomClass } from './baseCustomClass';
 
 // System Error
-export const ERROR_SERVER_ERROR:string = 'ERR_SERVER_ERROR';
+export const ERROR_SERVER_ERROR: string = 'ERR_SERVER_ERROR';
 // Input or Token error
-export const ERROR_CODE_INVALID_INPUT:string = 'ERROR_CODE_INVALID_INPUT';
-export const ERROR_CODE_INVALID_TOKEN:string = 'ERROR_CODE_INVALID_TOKEN';
-export const ERROR_CODE_NO_AUTHORIZE:string = 'ERROR_CODE_NO_AUTHORIZE';
-export const ERROR_CODE_MYSQL_CONNECTION:string = 'ERROR_CODE_MYSQL_CONNECTION';
-export const ERROR_CODE_NO_AUTHORIZE_IN_HEADER:string = 'ERROR_CODE_NO_AUTHORIZE_IN_HEADER';
-export const ERROR_CODE_REFRESH_EXPIRE:string = 'ERROR_CODE_REFRESH_EXPIRE';
-export const ERROR_CODE_INVALID_REFRESH_TOKEN:string = 'ERROR_CODE_INVALID_REFRESH_TOKEN';
-export const ERROR_CODE_ACCESS_EXPIRE:string = 'ERROR_CODE_ACCESS_EXPIRE';
-export const ERROR_CODE_INVALID_ACCESS_TOKEN:string = 'ERROR_CODE_INVALID_ACCESS_TOKEN';
+export const ERROR_CODE_INVALID_INPUT: string = 'ERROR_CODE_INVALID_INPUT';
+export const ERROR_CODE_INVALID_TOKEN: string = 'ERROR_CODE_INVALID_TOKEN';
+export const ERROR_CODE_NO_AUTHORIZE: string = 'ERROR_CODE_NO_AUTHORIZE';
+export const ERROR_CODE_MYSQL_CONNECTION: string =
+  'ERROR_CODE_MYSQL_CONNECTION';
+export const ERROR_CODE_NO_AUTHORIZE_IN_HEADER: string =
+  'ERROR_CODE_NO_AUTHORIZE_IN_HEADER';
+export const ERROR_CODE_REFRESH_EXPIRE: string = 'ERROR_CODE_REFRESH_EXPIRE';
+export const ERROR_CODE_INVALID_REFRESH_TOKEN: string =
+  'ERROR_CODE_INVALID_REFRESH_TOKEN';
+export const ERROR_CODE_ACCESS_EXPIRE: string = 'ERROR_CODE_ACCESS_EXPIRE';
+export const ERROR_CODE_INVALID_ACCESS_TOKEN: string =
+  'ERROR_CODE_INVALID_ACCESS_TOKEN';
 
 // User error list
-export const ERROR_CODE_USER_DUPLICATE_EMAIL:string = 'ERR_USER_DUPLICATE_EMAIL';
+export const ERROR_CODE_USER_DUPLICATE_EMAIL: string =
+  'ERR_USER_DUPLICATE_EMAIL';
 
 export class Error extends BaseCustomClass {
+
+  public static transformErrorFromAxios(returnError: any): Error {
+    if (!returnError.response || !returnError.response.data) {
+      return new Error(ERROR_CODE_NETWORK_ERROR, 'Network Error');
+    } else {
+      return returnError.response.data;
+    }
+  }
+  
   public code: string;
   public message: string;
 
